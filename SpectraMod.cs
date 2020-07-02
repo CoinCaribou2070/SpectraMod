1aecd17b7099e3c2d2d7250cf51f2eb816d5306e
Stevie <xxlennygamerxx@gmail.com>
2020-07-01 21:54:06 -0700

    "better" promode rarity code

--- SpectraMod.cs
+++ SpectraMod.cs
@@ -28,14 +28,18 @@ namespace SpectraMod
 			SkyManager.Instance["SpectraMod:DoomSlime"] = new DoomSky();
 			IL.Terraria.Main.DrawMenu += AddProfessionalMode;
 
-			On.Terraria.GameContent.UI.Elements.UIWorldListItem.DrawSelf += ProfessionalText;
 			On.Terraria.Main.DrawInterface_35_YouDied += YouDiedL;
+			On.Terraria.Main.MouseText += Main_MouseText;
+
 			On.Terraria.Player.DropCoins += DropAllYourCoins;
+
             On.Terraria.GameContent.UI.ItemRarity.Initialize += ItemRarity_Initialize;
             On.Terraria.GameContent.UI.ItemRarity.GetColor += ItemRarity_GetColor;
-            On.Terraria.Main.MouseText += Main_MouseText;
+
             On.Terraria.ItemText.NewText += ItemText_NewText;
             On.Terraria.ItemText.Update += ItemText_Update;
+
+			On.Terraria.GameContent.UI.Elements.UIWorldListItem.DrawSelf += ProfessionalText;
 
 			ItemRarity.Initialize();
 		}
